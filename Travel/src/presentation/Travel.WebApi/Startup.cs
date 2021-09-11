5a9c056c01ebd7aff73f548ffba432bdc08e9e48
Anderson Phiri <phirianderson23@gmail.com>
2021-09-11 15:23:41 +0800

    added intial migration

--- Travel/src/presentation/Travel.WebApi/Startup.cs
+++ Travel/src/presentation/Travel.WebApi/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Travel.Data.Contexts;
 
 namespace Travel.WebApi
 {
@@ -26,7 +28,9 @@ namespace Travel.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddDbContext<TravelDbContext>(options =>
+                  options
+                   .UseSqlite("Data Source=TravelTourDatabase.sqlite3"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
