4da44944f32d49fa3a74ebbc3c58d72b6cd0700f
Anderson Phiri <phirianderson23@gmail.com>
2021-09-19 20:00:14 +0800

    added tour module to the vue store

---
+++ Travel/src/presentation/vue-app/src/store/tour/mutations.js
@@ -0,0 +1,10 @@
+import * as types from  './types';
+const mutations = {
+    [types.GET_TOUR_LISTS](state, lists) {
+        state.lists = lists;
+    },
+    [types.LOADING_TOUR](state, value) {
+        state.loading = value;
+    }
+};
+export default mutations;
---
+++ Travel/src/presentation/vue-app/src/store/tour/state.js
@@ -0,0 +1,5 @@
+const state = {
+    lists: [],
+    loading: false,
+};
+export default state;
